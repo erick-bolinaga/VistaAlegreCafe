99f9bf7ebb20e058ae5a08b9828edb6047620077
erick_bolinaga <habanero01@gmail.com>
2025-02-23 15:42:36 +0200

    Configure routing for GitHub Pages deployment with base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,12 +6,15 @@ import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 import Layout from "@/components/Layout";
 
-function Router() {
+function AppRouter() {
+  const base = "/VistaAlegreCafe"
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={base}>
+      <Switch>
+        <Route path={`${base}/`} component={Home} />
+        <Route path={`${base}/*`} component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
@@ -19,7 +22,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
-        <Router />
+        <AppRouter />
       </Layout>
       <Toaster />
     </QueryClientProvider>
